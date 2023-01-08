a5219ea61b99449804e9d055ef137225ded81ce7
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2023-01-08 20:20:18 +0530

    22 Creating the CLI

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,15 @@
-import { Command } from 'commander';
+import path from "path";
+import { Command } from "commander";
+import { serve } from "local-api";
+
+// square brackets([]) denote that the value is optional
+// angled brackets(<>) denote that the value is required
 
 export const serveCommand = new Command()
-  .command('serve')
-  .description('Open a file for editing')
-  .action(() => {
-    console.log('Getting ready to serve a file');
+  .command("serve [filename]")
+  .description("Open a file for editing")
+  .option("-p, --port <number>", "Port to run server on", "4005")
+  .action((filename = "notebook.js", options: { port: string }) => {
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
---
+++ readme.md
@@ -0,0 +1,4 @@
+## "lerna Commands"
+
+"lerna bootstrap":"npm install",
+"lerna add <package>":"installs a package we can add scope as well"
